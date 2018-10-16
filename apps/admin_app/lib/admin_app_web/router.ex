744d70f9c00c56abaac725bbfd0ab068703dfee0
arjun289 <arjun289singh@gmail.com>
2018-10-16 14:38:44 +0530

    Add manage shipping-policy feature(#278)
    The admin should be able to setup rules for shipping category.
    These rules are used to calculate shipping cost when shipping
    method is set for the pacakge in the order.
    
    Changes
    - Added schema and migration for modelling needed to add
      the feature. The new enities are `ShippingRule`,
      `ShippingIdentifier`
    - Added logic for shipping calculator which calculates
     the cost based on the rules.
    - Added views in admin panel to manage the rules.

--- apps/admin_app/lib/admin_app_web/router.ex
+++ apps/admin_app/lib/admin_app_web/router.ex
@@ -94,6 +94,11 @@ defmodule AdminAppWeb.Router do
       ProductController,
       :delete_property
     )
+
+    # get("/shipping-policy", ShippingPolicyController, :index)
+    get("/shipping-policy/new", ShippingPolicyController, :new)
+    get("/shipping-policy/:id/edit", ShippingPolicyController, :edit)
+    put("/shipping-policy/:id", ShippingPolicyController, :update)
   end
 
   scope "/", AdminAppWeb do
