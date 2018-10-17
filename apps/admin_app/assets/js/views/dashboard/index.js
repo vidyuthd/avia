9d579fb7a4e081cf168e53b87bf250ac5dae7e3d
anantanant2015 <anantanant2015@gmail.com>
2018-10-17 12:18:55 +0530

    Adds datapoints use in js, sent from controller

--- apps/admin_app/assets/js/views/dashboard/index.js
+++ apps/admin_app/assets/js/views/dashboard/index.js
@@ -2,44 +2,36 @@ import MainView from '../main';
 
 export default class View extends MainView {
     mount() {
-      super.mount();
+        super.mount();
 
-      var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue"],
-        datasets: [{
-            label: '# of Votes',
-            data: [{x:'2016-12-25', y:20}, {x:'2016-12-26', y:10}],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: barchart.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: barchart.data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
                 }
-            }]
-        }
-    }
-});
+            }
+        });
 
-      console.log('DashboardIndexView mounted');
+        console.log('DashboardIndexView mounted');
     }
 
     unmount() {
-      super.unmount();
+        super.unmount();
 
-      console.log('DashboardIndexView unmounted');
+        console.log('DashboardIndexView unmounted');
     }
 }
--- apps/admin_app/lib/admin_app_web/controllers/dashboard_controller.ex
+++ apps/admin_app/lib/admin_app_web/controllers/dashboard_controller.ex
@@ -2,6 +2,17 @@ defmodule AdminAppWeb.DashboardController do
   use AdminAppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    barchart = %{
+      labels: ["2016-12-25", "2016-12-26", "2016-12-27", "2016-12-28", "2016-12-29"],
+      data: [
+        %{x: "2016-12-25", y: 20},
+        %{x: "2016-12-26", y: 10},
+        %{x: "2016-12-27", y: 20},
+        %{x: "2016-12-28", y: 30},
+        %{x: "2016-12-29", y: 100}
+      ]
+    }
+
+    render(conn, "index.html", barchart: barchart)
   end
 end
